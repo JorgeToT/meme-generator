2ec74c7392bdc48c6672078b0c741098f5817bdc
Jorge Aguilar <jorge.agvel@gmail.com>
2022-04-21 22:48:13 -0500

    Adding function to get a random meme image

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,9 +1,34 @@
+import memeData from "./../memesData.js";
+
 function Meme() {
+  let memeUrl;
+  function getImage() {
+    const memesArray = memeData.data.memes;
+    memeUrl = memesArray[Math.floor(Math.random() * memesArray.length)].url;
+    console.log(memeUrl);
+  }
   return (
-    <main className="form">
-      <input type="text" name="" id="" className="form--input" placeholder="Top Text"/>
-      <input type="text" name="" id="" className="form--input" placeholder="Bottom Text"/>
-      <button className="form--button">Get a new meme image 🖼️</button>
+    <main>
+      <p>{memeUrl}</p>
+      <div className="form">
+        <input
+          type="text"
+          name=""
+          id=""
+          className="form--input"
+          placeholder="Top Text"
+        />
+        <input
+          type="text"
+          name=""
+          id=""
+          className="form--input"
+          placeholder="Bottom Text"
+        />
+        <button className="form--button" onClick={getImage}>
+          Get a new meme image 🖼️
+        </button>
+      </div>
     </main>
   );
 }
